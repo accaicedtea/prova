70dd77cab4161a5912c672b51bb196662b410ade
accaicedtea <107927176+accaicedtea@users.noreply.github.com>
2022-11-12 19:42:14 +0100

    Create Acq copy.java
    copia acq

---
+++ Acq/src/acq/Acq copy.java
@@ -0,0 +1,14 @@
+package acq;
+
+import java.util.Scanner;
+
+public class InputClass {
+	private Scanner sc= new Scanner(System.in); 
+	public static String inputString(){
+		String s;//res vaule
+		s= sc.nextLine();
+		if()
+
+	}
+
+}
